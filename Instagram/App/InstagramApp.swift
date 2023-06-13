9f0725dd7eeb4891f0e6bea96ddfb0803d410ae9
Leo Villalobos <villaleobos@gmail.com>
2023-06-13 14:07:57 -0700

    Initial Commit

---
+++ Instagram/App/InstagramApp.swift
@@ -0,0 +1,19 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Leonardo Villalobos on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+	
+}
---
+++ Instagram/Core/Views/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Leonardo Villalobos on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundColor(.accentColor)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+	
+}
+
+struct ContentView_Previews: PreviewProvider {
+	
+	static var previews: some View {
+		ContentView()
+	}
+	
+}
