330371a7ab1677c56e635f1082c565638d976f03
Leo Villalobos <villaleobos@gmail.com>
2023-06-26 10:17:23 -0700

    Create NavHeader view modifier within Profile.

--- Instagram/Core/Views/Profile/Profile.swift
+++ Instagram/Core/Views/Profile/Profile.swift
@@ -7,6 +7,66 @@
 
 import SwiftUI
 
+// MARK: - NavHeader
+
+/// The navigation bar seen at the top of the view. Shows the username and two buttons: one to create a new
+/// post, and the other to view settings and other options.
+fileprivate struct NavHeader: ViewModifier {
+	let username: UserProfile.Username
+	
+	func body(content: Content) -> some View {
+		content
+			.toolbar {
+				ToolbarItem(placement: .navigationBarLeading) {
+					Button {
+						// Show switch account sheet
+					} label: {
+						HStack {
+							Image(systemName: "lock")
+								.resizable()
+								.aspectRatio(contentMode: .fit)
+								.frame(width: 15, height: 15)
+								.fontWeight(.semibold)
+							Text(username.description)
+								.bold()
+								.font(.title2)
+							Image(systemName: "chevron.down")
+								.resizable()
+								.aspectRatio(contentMode: .fit)
+								.frame(width: 10, height: 10)
+								.fontWeight(.semibold)
+						}
+					}
+				}
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button {
+						// Show create sheet
+					} label: {
+						Image(systemName: "plus.app")
+							.resizable()
+							.aspectRatio(contentMode: .fit)
+							.frame(width: 22, height: 22)
+					}
+				}
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button {
+						// Show account options
+					} label: {
+						Image(systemName: "line.3.horizontal")
+							.resizable()
+							.aspectRatio(contentMode: .fit)
+							.frame(width: 22, height: 22)
+					}
+				}
+			}
+			.toolbarBackground(.background, for: .navigationBar)
+	}
+	
+}
+
+// MARK: - Profile
+
+/// The parent view for the Profile screen tab.
 struct Profile: View {
 	
 	@EnvironmentObject var database: Database
@@ -15,12 +75,12 @@ struct Profile: View {
 	
 	private var screenNavBarOffset: CGFloat { (UIScreen.main.bounds.height / 2) - 70 }
 	
+	/// Bottom navigation bar for the App. Switches content based on selected tab.
 	var screenNavBar: some View {
 		HStack {
 			ForEach(ScreenTab.allCases) { tab in
 				Spacer()
 				Button {
-					// Switch screen
 					withAnimation(.spring()) {
 						selectedScreenTab = tab
 					}
@@ -51,11 +111,12 @@ struct Profile: View {
 				screenNavBar
 					.offset(y: screenNavBarOffset)
 			}
-			.modifier(ProfileNavigationViewModifier(username: database.userProfile.username))
+			.modifier(NavHeader(username: database.userProfile.username))
 		}
 		.foregroundColor(.primaryText)
 	}
 	
+	/// Main content of the Profile view.
 	var content: some View {
 		ScrollView {
 			VStack(spacing: 2.5) {
@@ -101,6 +162,8 @@ struct Profile: View {
 	}
 	
 }
+
+// MARK: - Previews
 
 struct Profile_Previews: PreviewProvider {
 	
