2b812a6083fff30aab12c5fc833285ca334463cb
Leo Villalobos <villaleobos@gmail.com>
2023-06-29 16:28:30 -0700

    Remove images and replace with a photo URL.

--- Instagram/Core/Views/Profile/ProfilePhotoButton.swift
+++ Instagram/Core/Views/Profile/ProfilePhotoButton.swift
@@ -5,53 +5,61 @@
 //  Created by Leonardo Villalobos on 6/13/23.
 //
 
+import Shimmer
 import SwiftUI
 
 struct ProfilePhotoButton: View {
 	
+	@EnvironmentObject var database: Database
 	@State private var hasStoryPost = true
 	
 	var body: some View {
 		Button {
 			// Show photos library to post to story
 		} label: {
-			Image("ProfilePhoto")
-				.renderingMode(.original)
-				.resizable()
-				.aspectRatio(contentMode: .fit)
-				.clipShape(Circle())
-				.overlay {
-					if hasStoryPost {
-						Circle()
-							.stroke(
-								.linearGradient(
-									colors: [.purple, .pink, .yellow],
-									startPoint: .topTrailing,
-									endPoint: .bottomLeading
-								),
-								lineWidth: 2
-							)
-							.scaleEffect(1.1)
-					}
-				}
-				.overlay {
+			AsyncImage(url: database.current.photoURL) { image in
+				image
+					.resizable()
+					.aspectRatio(1, contentMode: .fit)
+					.clipShape(Circle())
+			} placeholder: {
+				Circle()
+					.fill(Color(uiColor: .lightGray))
+					.shimmering(duration: 1)
+					.aspectRatio(contentMode: .fit)
+			}
+			.overlay {
+				if hasStoryPost {
 					Circle()
-						.stroke(Color.lightGray, lineWidth: 1.5)
-					Image(systemName: "plus")
-						.fontWeight(.semibold)
-						.foregroundColor(.white)
-						.background(
-							Circle()
-								.foregroundColor(.systemBlue)
-								.frame(width: 25, height: 25)
-								.overlay {
-									Circle()
-										.stroke(Color.background, lineWidth: 2)
-								}
+						.stroke(
+							.linearGradient(
+								colors: [.purple, .pink, .yellow],
+								startPoint: .topTrailing,
+								endPoint: .bottomLeading
+							),
+							lineWidth: 2
 						)
-						.offset(x: 37, y: 37)
+						.scaleEffect(1.1)
 				}
-				.frame(width: 100, height: 100)
+			}
+			.overlay {
+				Circle()
+					.stroke(Color.lightGray, lineWidth: 1.5)
+				Image(systemName: "plus")
+					.fontWeight(.semibold)
+					.foregroundColor(.white)
+					.background(
+						Circle()
+							.foregroundColor(.systemBlue)
+							.frame(width: 25, height: 25)
+							.overlay {
+								Circle()
+									.stroke(Color.background, lineWidth: 2)
+							}
+					)
+					.offset(x: 37, y: 37)
+			}
+			.frame(width: 100, height: 100)
 		}
 	}
 	
@@ -61,6 +69,7 @@ struct ProfilePhotoButton_Previews: PreviewProvider {
 	
 	static var previews: some View {
 		ProfilePhotoButton()
+			.environmentObject(Database())
 	}
 	
 }
--- Instagram/Models/Profile.swift
+++ Instagram/Models/Profile.swift
@@ -12,6 +12,15 @@ struct Profile: Codable, Identifiable {
 	var id: UInt
 	var name: String
 	var username: String
+	
+	private var photoUrlString: String
+	var photoURL: URL {
+		guard let url = URL(string: photoUrlString) else {
+			fatalError("Invalid url '\(photoUrlString)'.")
+		}
+		return url
+	}
+	
 	var bio: String
 	var bioLink: String?
 	var numPosts: Int64 { .init(posts.count) }
